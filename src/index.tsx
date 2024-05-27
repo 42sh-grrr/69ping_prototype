aa80dbb5ad41a31e5d68f06ef278c2feb94775e5
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-27 21:29:55 +0200

    smooth criminal

--- src/index.tsx
+++ src/index.tsx
@@ -13,18 +13,28 @@ root.render(<App />);
 (window as any).customAngle = null;
 // (window as any).customAngle = -45;
 
+let targetAngle = 0;
+let currentAngle = 0;
+
+let oldT = 0;
 const frame = (t: number) => {
   t /= 1000;
-  const angle = (window as any).customAngle ?? Math.sin(t / 10) * 45;
-  document.documentElement.style.setProperty('--angle', `${angle}deg`);
+  targetAngle = (window as any).customAngle ?? Math.sin(t) * 45;
 
-  if (angle > 0) {
+  const dt = t - oldT;
+  const prop = Math.min(Math.max(dt * 10, 0), 1);
+  currentAngle = (1 - prop) * currentAngle + prop * targetAngle;
+
+  document.documentElement.style.setProperty('--angle', `${currentAngle}deg`);
+
+  if (currentAngle > 0) {
     document.body.classList.remove("angle-inverted");
   }
   else {
     document.body.classList.add("angle-inverted");
   }
   
+  oldT = t;
   requestAnimationFrame(frame);
 };
 requestAnimationFrame(frame);
