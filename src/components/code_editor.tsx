0da3b5e0f322b547f5a6dcedee9fc73f8e7e25c1
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-30 01:30:12 +0200

    disable spell check

--- src/components/code_editor.tsx
+++ src/components/code_editor.tsx
@@ -7,7 +7,7 @@ export type Props = React.PropsWithChildren & {
 
 export function CodeEditor(props: Props) {
   return <div className={classes["code-editor"]}>
-    <div className={classes["the-code"] + " rotated-container"} contentEditable={true}>
+    <div className={classes["the-code"] + " rotated-container"} contentEditable={true} spellCheck={false}>
       {props.code.split("\n")
         .map((line, i) => <pre key={i} className={classes["line"] + " rotated-element"}>{line}</pre>)}
     </div>
