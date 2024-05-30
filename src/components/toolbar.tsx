fa7c3c10a4fb3e83a095b2d3495d32e52be096e4
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-30 06:47:09 +0200

    rotate letters

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -4,11 +4,11 @@ import * as classes from "./toolbar.module.scss";
 export function ToolBar() {
   return <div className={classes["tool-bar"]}>
     <ul>
-      <li>Fichier</li>
-      <li>Bref</li>
-      <li>Jsp</li>
-      <li>Les</li>
-      <li>Trucs</li>
+      <li><span>F</span><span>i</span><span>c</span><span>h</span><span>i</span><span>e</span><span>r</span></li>
+      <li><span>B</span><span>r</span><span>e</span><span>f</span></li>
+      <li><span>J</span><span>s</span><span>p</span></li>
+      <li><span>L</span><span>e</span><span>s</span></li>
+      <li><span>T</span><span>r</span><span>u</span><span>c</span><span>s</span></li>
     </ul>
   </div>;
 }
