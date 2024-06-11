b01e304af4b5e866225cd9ebb719ec8afbb360e9
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-06-11 17:31:48 +0200

    better

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,14 +1,31 @@
 import * as React from "react";
 import * as classes from "./toolbar.module.scss";
 
+function Wordise(props: { children: string }) {
+  return <span>
+    {props.children.split("").map((c, i) => <span key={i} className={classes.letter}>{c}</span>)}
+  </span>;
+}
+
 export function ToolBar() {
+
   return <div className={classes["tool-bar"]}>
     <ul>
-      <li><span>F</span><span>i</span><span>c</span><span>h</span><span>i</span><span>e</span><span>r</span></li>
-      <li><span>B</span><span>r</span><span>e</span><span>f</span></li>
-      <li><span>J</span><span>s</span><span>p</span></li>
-      <li><span>L</span><span>e</span><span>s</span></li>
-      <li><span>T</span><span>r</span><span>u</span><span>c</span><span>s</span></li>
+      <li>
+        <Wordise>Configuration</Wordise>
+      </li>
+      <li>
+        <Wordise>Fichier</Wordise>
+      </li>
+      <li>
+        <Wordise>Et</Wordise>
+      </li>
+      <li>
+        <Wordise>D'autre</Wordise>
+      </li>
+      <li>
+        <Wordise>Trucs</Wordise>
+      </li>
     </ul>
   </div>;
 }
