d66b8d89def29d4589d16b88a1775647e8fff170
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-06-21 15:34:20 +0200

    bonjour

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,38 +1,62 @@
-import * as React from "react";
+import { PropsWithChildren, useCallback } from "react";
 import { createPortal } from "react-dom";
 import * as classes from "./toolbar.module.scss";
 import { useIsOpen } from "./settings";
 
-function Wordise(props: { children: string }) {
-  return <span>
-    {props.children.split("").map((c, i) => <span key={i} className={classes.letter}>{c}</span>)}
-  </span>;
+function ToolBarElement(props: PropsWithChildren) {
+  return <li className={classes["toolbar-el"]}>
+    {props.children}
+  </li>;
 }
 
 export function ToolBar() {
   const [isConfigOpen, setIsConfigOpen] = useIsOpen();
 
-  const open = React.useCallback(() => {
+  const open = useCallback(() => {
     setIsConfigOpen(true);
   }, []);
 
   return <div className={classes["tool-bar"]}>
     <ul>
       <li onClick={open}>
-        <Wordise>Configuration</Wordise>
+        Configuration
       </li>
-      <li>
-        <Wordise>Fichier</Wordise>
-      </li>
-      <li>
-        <Wordise>Et</Wordise>
-      </li>
-      <li>
-        <Wordise>D'autre</Wordise>
-      </li>
-      <li>
-        <Wordise>Trucs</Wordise>
-      </li>
+      <ToolBarElement>
+        <summary>Fichier</summary>
+        <ul>
+          <li>Open</li>
+          <li>Close</li>
+          <li>Delete</li>
+          <li>Nuke</li>
+        </ul>
+      </ToolBarElement>
+      <ToolBarElement>
+        <summary>Edit</summary>
+        <ul>
+          <li>Copy</li>
+          <li>Paste</li>
+          <li>Cheat</li>
+          <li>lol</li>
+        </ul>
+      </ToolBarElement>
+      <ToolBarElement>
+        <summary>Bref</summary>
+        <ul>
+          <li>Copy</li>
+          <li>Paste</li>
+          <li>Cheat</li>
+          <li>lol</li>
+        </ul>
+      </ToolBarElement>
+      <ToolBarElement>
+        <summary>Autre chose</summary>
+        <ul>
+          <li>Copy</li>
+          <li>Paste</li>
+          <li>Cheat</li>
+          <li>lol</li>
+        </ul>
+      </ToolBarElement>
     </ul>
   </div>;
 }
