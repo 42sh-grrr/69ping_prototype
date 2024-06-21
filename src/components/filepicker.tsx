0435490a684d57ade7f77dceb6f6b7aa4f419276
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-06-21 02:06:04 +0200

    type defs for variables

--- src/components/filepicker.tsx
+++ src/components/filepicker.tsx
@@ -1,11 +1,10 @@
-import * as React from "react";
 import * as classes from "./filepicker.module.scss";
 
 export function FilePicker() {
   return (
     <div className={classes["file-picker"]}>
-      <ul className="rotated-container">
-        {new Array(5).fill(null).map((_, i) => <li key={i} className="rotated-element">
+      <ul>
+        {new Array(5).fill(null).map((_, i) => <li key={i}>
           File number {i}
         </li>)}
       </ul>
