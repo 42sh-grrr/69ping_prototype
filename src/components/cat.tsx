07fabbb5ed2b2347fed8c49fbe026976dbb8e2d0
Louis-Mael Derval <louis-mael.derval@epita.fr>
2024-06-26 12:07:02 +0200

    feat(cat): still working on the cat

--- src/components/cat.tsx
+++ src/components/cat.tsx
@@ -14,6 +14,7 @@ export function Cat() {
   const [cat, setCat] = useState(null as string | null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+  const frontRef = useRef<HTMLDivElement | null>(null);
 
   const fetchNewImage = useCallback(async (force: boolean = false) => {
     if (!force && cat === null)
@@ -44,9 +45,35 @@ export function Cat() {
     try {
       const audio = new Audio(LINKS[Math.floor(Math.random() * LINKS.length)]);
       audio.play();
+      setTimeout(() => {
+        if (frontRef.current != null) {
+          console.log("--w: " + getComputedStyle(frontRef.current).getPropertyValue("--w"));
+          console.log("--wx: " + getComputedStyle(frontRef.current).getPropertyValue("--wx"));
+          console.log("--wy: " + getComputedStyle(frontRef.current).getPropertyValue("--wy"));
+
+          console.log("--h: " + getComputedStyle(frontRef.current).getPropertyValue("--h"));
+          console.log("--hx: " + getComputedStyle(frontRef.current).getPropertyValue("--hx"));
+          console.log("--hy: " + getComputedStyle(frontRef.current).getPropertyValue("--hy"));
+
+          console.log("--d: " + getComputedStyle(frontRef.current).getPropertyValue("--d"));
+          console.log("--dx: " + getComputedStyle(frontRef.current).getPropertyValue("--dx"));
+          console.log("--dy: " + getComputedStyle(frontRef.current).getPropertyValue("--dy"));
+
+          console.log("--mx: " + getComputedStyle(frontRef.current).getPropertyValue("--mx"));
+          console.log("--my: " + getComputedStyle(frontRef.current).getPropertyValue("--my"));
+
+          console.log("--beta: " + getComputedStyle(frontRef.current).getPropertyValue("--beta"));
+          console.log("--gamma: " + getComputedStyle(frontRef.current).getPropertyValue("--gamma"));
+
+          console.log("--yi: " + getComputedStyle(frontRef.current).getPropertyValue("--yi"));
+          console.log("--xj: " + getComputedStyle(frontRef.current).getPropertyValue("--xj"));
+
+          console.log("--x: " + getComputedStyle(frontRef.current).getPropertyValue("--x"));
+          console.log("--y: " + getComputedStyle(frontRef.current).getPropertyValue("--y"));
+        }
+      }, 1000);
     }
-    catch
-    {
+    catch {
       console.log("Could not play sound");
     }
   }, []);
@@ -71,10 +98,10 @@ export function Cat() {
         setHeight(image.height);
 
         playSound();
-      }}/>
-      <div className={classes.front} style={{
+      }} />
+      <div className={classes.front} ref={frontRef} style={{
         backgroundImage: `url(${cat ?? ""})`
-      }}/>
+      }} />
     </>;
   }
 
@@ -97,9 +124,10 @@ export function Cat() {
   }, []);
 
   return <div className={classes.container} ref={catRef} onClick={() => fetchNewImage()} style={{
-      "--img-width": `${width}px`,
-      "--img-height": `${height}px`
-    }}>
+    "--img-width": `${width}px`,
+    "--img-height": `${height}px`,
+    "--beta": `${height / width}`
+  }}>
     {in_}
   </div>;
 }
